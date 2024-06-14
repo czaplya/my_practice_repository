1394e18a0eff3e59b9fbeb7ff049547c84c5d7d0
O Sh <137957204+OlenaShallashna@users.noreply.github.com>
2024-06-14 10:21:14 -0700

    added git ignore and lovely message (#6)
    Co-authored-by: olena8420 <shalashnaolena@gmail.com>

--- src/test/java/TestClassFromOlena.java
+++ src/test/java/TestClassFromOlena.java
@@ -2,6 +2,7 @@ public class TestClassFromOlena {
 //new class added
 public static void main(String[] args) {
     System.out.println("Hello world from Daniela");
+    System.out.println("I love JAVA");
 
 }
 }
