effdfc3bbbd0378e14ace32ee33a18b071261208
olena8420 <shalashnaolena@gmail.com>
2024-06-14 10:28:07 -0700

    private Message to Daniela

--- src/test/java/TestClassFromOlena.java
+++ src/test/java/TestClassFromOlena.java
@@ -4,5 +4,7 @@ public static void main(String[] args) {
     System.out.println("Hello world from Daniela");
     System.out.println("I love JAVA");
 
+    System.out.println("Olena's private message to Dani");
+
 }
 }
