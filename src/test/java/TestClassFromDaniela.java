b6fb394358c268a59dcfa76e898b245afaaa230c
12017 <danielacruz367@gmail.com>
2024-06-14 12:55:33 -0400

    Added my own class and a greetings

---
+++ src/test/java/TestClassFromDaniela.java
@@ -0,0 +1,5 @@
+public class TestClassFromDaniela {
+    public static void main(String[] args) {
+        System.out.println("Hey dear ladies");
+    }
+}
--- src/test/java/TestClassFromOlena.java
+++ src/test/java/TestClassFromOlena.java
@@ -1,3 +1,7 @@
 public class TestClassFromOlena {
 //new class added
+public static void main(String[] args) {
+    System.out.println("Hello world from Daniela");
+
+}
 }
